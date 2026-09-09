02a7d4f776656fdf760d9e3ddf126c3c6f615d00
Xuntacdor <nhatquang1223@gmail.com>
2026-09-09 21:41:44 +0700

    feat(subscription): add promo code migration

---
+++ NomiWrite.Backend/src/Services/Subscription/NomiWrite.Subscription.Infrastructure/Migrations/20260909014705_AddPromoCode.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NomiWrite.Subscription.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPromoCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
