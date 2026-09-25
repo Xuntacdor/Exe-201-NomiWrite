ffbbc3cec7027549cee8b65749094ebfe8d337dc
Xuntacdor <nhatquang1223@gmail.com>
2026-09-25 10:22:40 +0700

    chore(frontend): resolve next config conflict markers

--- NomiWrite.Frontend/next.config.ts
+++ NomiWrite.Frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< Updated upstream
-  /* config options here */
-=======
   devIndicators: false,
->>>>>>> Stashed changes
 };
 
 export default nextConfig;
